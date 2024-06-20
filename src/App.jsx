42e1b64ca4c09e035823388cfa17582d0df03d6d
leeminwook1 <dlalsldnr2303@naver.com>
2024-06-20 17:13:12 +0900

    json 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Todoinput from './components/Todoinput';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: uuidv4(),
-      completed: true,
-      title: '리액트 공부하기',
-    },
-    {
-      id: uuidv4(),
-      completed: false,
-      title: '축구 연습하기',
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/todos')
+      .then((db) => db.json())
+      .then((data) => setTodos(data));
+  }, []);
 
   const addTodo = (newTodo) => {
     const updatedTodos = [
